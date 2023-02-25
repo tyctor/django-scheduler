1cf4c19250819276ee31ba67dfb74db400f6ef3c
Viktor <viktor.vraniak@gmail.com>
2023-02-25 22:12:46 +0100

    Add django-modeltranslation support to Event and Occurence models

---
+++ schedule/translation.py
@@ -0,0 +1,9 @@
+from modeltranslation.translator import translator, TranslationOptions
+from schedule.models.events import Event, Occurrence
+
+
+class TitleDescriptionTranslationOptions(TranslationOptions):
+    fields = ('title', 'description', )
+
+translator.register(Event, TitleDescriptionTranslationOptions)
+translator.register(Occurrence, TitleDescriptionTranslationOptions)
