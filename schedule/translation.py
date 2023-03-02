a77fbee7f39654bc6c6890810b2e29b47384ef96
Viktor <viktor.vraniak@gmail.com>
2023-03-02 16:39:25 +0100

    Handle translation import

--- schedule/translation.py
+++ schedule/translation.py
@@ -1,9 +1,13 @@
-from modeltranslation.translator import translator, TranslationOptions
-from schedule.models.events import Event, Occurrence
+import warnings
+try:
+    from modeltranslation.translator import translator, TranslationOptions
+    from schedule.models.events import Event, Occurrence
 
 
-class TitleDescriptionTranslationOptions(TranslationOptions):
-    fields = ('title', 'description', )
+    class TitleDescriptionTranslationOptions(TranslationOptions):
+        fields = ('title', 'description', )
 
-translator.register(Event, TitleDescriptionTranslationOptions)
-translator.register(Occurrence, TitleDescriptionTranslationOptions)
+    translator.register(Event, TitleDescriptionTranslationOptions)
+    translator.register(Occurrence, TitleDescriptionTranslationOptions)
+except Exception as exc:
+    warnings.warn("translations not installed")
